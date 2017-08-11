b535854569ae0e00ca4df7b53e4e9e5c8ac3a9fd
BATLAPTOP\unkno <unknow0912@outlook.com>
2017-08-11 20:02:43 +0700

    no message

--- buoi 4/home1.py
+++ buoi 4/home1.py
@@ -1,12 +1,12 @@
 user_str=("ThiS is String with Upper and lower case Letters")
-
+not_word=[",",".",";",":","-","(",")","'","\""," "]
     
     
 def count_all(text):
     text = text.lower()
     counts = {}
     for char in text:
-        if char == " ":
+        if char in not_word :
             continue
         if char not in counts:
             counts.setdefault(char,1)
@@ -15,6 +15,7 @@ def count_all(text):
     counts = sorted(counts.items())
     for i in counts:
         print(i[0],' ',i[1])
+        
 
 count_all(user_str)
 
