76117f3d7e609fd2f68ab874975be42aca041c30
BATLAPTOP\unkno <unknow0912@outlook.com>
2017-08-14 00:05:51 +0700

    ses7 homework

---
+++ buoi 7/home1_ses7/home1_ses7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace home1_ses7
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			float[] l = new float[]{ 1, 2, 3, 4 };
+		
+
+
+			foreach (int i in l)
+			{
+			
+				string binary = Convert.ToString (i, 2);
+				Console.WriteLine (binary);
+			}
+
+		}
+	}
+}
---
+++ buoi 7/home2_ses7/home2_ses7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace home2_ses7
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+			
+			List<int> allDivisors = new List<int> ();
+
+			int[] l = new int[]{ 1, 2, 3, 4, 5, 6 };
+		
+			for (int y = 0; y < l.Length; y++) 
+				
+			{
+				int a = l [y];
+				for (int i = a; i >0; i--) {
+					if (i % a == 0) {
+						allDivisors.Add(i);
+					}
+				}
+			}
+			Console.WriteLine (allDivisors);
+
+		}
+						
+	}
+}
+		
+	
+
