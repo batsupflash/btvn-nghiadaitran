e7b004e0af6f83e34a2ee458b5dcd4c0e2268919
BATLAPTOP\unkno <unknow0912@outlook.com>
2017-08-17 14:00:48 +0700

    no message

--- buoi 7/home1_ses7/home1_ses7/Program.cs
+++ buoi 7/home1_ses7/home1_ses7/Program.cs
@@ -6,17 +6,37 @@ namespace home1_ses7
 	{
 		public static void Main (string[] args)
 		{
-			float[] l = new float[]{ 1, 2, 3, 4 };
-		
-
-
-			foreach (int i in l)
-			{
-			
-				string binary = Convert.ToString (i, 2);
-				Console.WriteLine (binary);
+			//Convert to Binary
+			int numberDecimal = 10;
+			string binary = string.Empty;
+			int num = numberDecimal;
+			int remainder = 0;
+			while (numberDecimal>0) {
+				//Chia lay so du
+				remainder=numberDecimal % 2;
+				//Cong chuoi
+				binary = remainder.ToString() + binary;
+				numberDecimal = numberDecimal / 2;
 			}
+			Console.WriteLine ("Binary of "+ num + " is: "+binary);
 
+			//Highest commond factor
+			int a,b;
+			a = 0;
+			b = 180;
+
+			int i;
+			i = (a < b) ? a : b;
+
+			int hcf = 0;
+
+			for (int j = 1; j <= i; j++) {
+				if (a % j == 0 && b %j == 0) {
+					hcf = j;
+				}
+			
+			}
+			Console.WriteLine (hcf);
 		}
 	}
 }
